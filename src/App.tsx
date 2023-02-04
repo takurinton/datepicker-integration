653cc1c99e4609ee6b4b9d867cb7e2a98fb104e2
takurinton <takurinton@takurinton.dev>
2023-02-04 20:32:14 +0900

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import { createTheme, ThemeProvider } from "ingred-ui";
 import dayjs, { Dayjs } from "dayjs";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { DatePicker } from "./DatePicker";
 
 function App() {
   const theme = createTheme();
   const [date, setDate] = useState<Dayjs>(dayjs());
-  const actions = [
-    {
-      text: "今日",
-      onClick: () => {
-        setDate(dayjs());
+  const actions = useMemo(
+    () => [
+      {
+        text: "今日",
+        onClick: () => {
+          setDate(dayjs());
+        },
       },
-    },
-    {
-      text: "来週",
-      onClick: () => {
-        console.log("clicked");
-        setDate(dayjs().add(1, "week"));
+      {
+        text: "来週",
+        onClick: () => {
+          console.log("clicked");
+          setDate(dayjs().add(1, "week"));
+        },
       },
-    },
-  ];
+    ],
+    []
+  );
 
   return (
     <ThemeProvider theme={theme}>
